d0ab4065d943511dd4b3309fdd7cf2cfd05802b3
Sujal-282 <sujalfursule282@gmail.com>
2026-07-15 16:20:33 +0530

    Create HelloWorld.java

---
+++ Java/Day01/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Sujal.");
+        System.out.println("This is my Java Placement Journey.");
+    }
+}
